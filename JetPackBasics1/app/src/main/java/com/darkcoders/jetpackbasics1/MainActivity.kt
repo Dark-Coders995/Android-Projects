d8f4a9ce5f1b09eaa0b90165edbcfb9ff452780d
Dark$Coders <59391849+Dark-Coders995@users.noreply.github.com>
2022-04-19 12:38:40 +0530

    Updates

---
+++ JetPackBasics1/app/src/main/java/com/darkcoders/jetpackbasics1/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.darkcoders.jetpackbasics1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.darkcoders.jetpackbasics1.ui.theme.JetPackBasics1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackBasics1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetPackBasics1Theme {
+        Greeting("Android")
+    }
+}
